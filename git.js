0e3ee338afd229f828f81a6a36ea3bbc6ccc1a7f
18735185652 <592908853@qq.com>
2019-05-03 20:10:38 +0800

    git

---
+++ git.js
@@ -0,0 +1,52 @@
+// 配置用户（不配置用户不能提交代码）
+
+// git config --list 查看当前用户信息
+
+// git config --global user.name xxx
+// git config --hlobal user.email "你的邮箱"
+
+// 初始化git  一个项目初始化一次，不能嵌套着来
+// git init 告诉git哪个文件夹被git所管理
+// git status 查看git状态
+// git add . 添加到暂存区
+// git rm --cached 文件名或者 . -r 从暂存区移除
+// git commit -m "提交描述" 提交到历史区
+// git commit -a -m"文件描述" 一步到位 从工作区提交到暂存区与历史区
+// git log 查看提交历史记录
+// git reflog 查看所有历史记录 包括回滚
+// git reset --head HARD^ 回退上一版本
+// git reset --head 版本号 回退指定版本
+
+// git branch             查看分支
+// git branch 分支名       创建分支
+// git branch -D 分支名    删除分支
+// git checkout -b 分支名  查看并切换分支
+// git checkout 分支名     切换指定分支
+// git stash 暂存文件 分支有更改不能直接切换，提交更改或者暂存更改，暂存是用过渡区覆盖掉工作区
+// git stash pop 还原暂存的内容
+// git merge 分支名 合并分支
+
+
+// linux命令
+// pwd print working directory 查看当前工作目录
+// rm -rf 文件夹名  递归强制删除(小心使用)
+// rm 文件名 删除文件
+// mkdir 文件夹名 创建目录
+// touch 文件  创建文件
+// cd 目录名 改变路径
+// cd d: 改变所处盘符
+// ls -al 显示目录下所有文件(ls ：list show 的缩写 -al可以查看隐藏文件)
+
+// cat 文件名 查看文件内容
+// vim 文件名 编辑文件内容
+// 1.按 i insert插入
+// 2.输入内容
+// 3.Esc推出  shift+： wq 保存并退出 q！强制退出不保存
+
+
+
+
+
+
+
+
